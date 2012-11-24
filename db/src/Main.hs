8f712ee61980947bdfb1765df953a90af497508e
Eivind <eivind.jahren@gmail.com>
2012-11-24 19:19:17 +0100

    Added commandline options to CoffeW8DB

--- db/src/Main.hs
+++ db/src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveDataTypeable #-}
 module Main where
 import Database.MongoDB as M
 import Network.Browser
@@ -9,17 +9,36 @@ import System.Process as P
 import qualified Data.ByteString.Lazy as B
 import Measurement
 import DBHandling
+import System.Console.CmdArgs
 
-main = mainLoop 1
+data AppConfig = AppConfig {
+  runOnce :: Bool,
+  listenDomain :: String,
+  interval :: Int
+} deriving (Show, Data, Typeable)
 
+appconfig = AppConfig {
+  runOnce = False  &= help "When given the values are only queried for once",
+  listenDomain = "http://veiset.org:8183/" &= typ "DOMAIN", -- &= name "DOMAIN" &= argPos 0 ,
+  interval = 10 &= help "The number of to wait before requerying the values"
+} &= summary "CoffeW8DB v 0.1" &= program "coffeW8DB"
 
-mainLoop i = do
+main = do 
+  inp <- cmdArgs appconfig
+  let domain = listenDomain inp
+  let intrv = interval inp
+  if runOnce inp 
+   then
+    mainBody 1 domain intrv (const (return ()))
+   else
+    mainLoop domain intrv 1
+
+mainBody t domain intrv callback = do
   (_, rsp)
      <- browse $ do
            setAllowRedirects True
-           request $ getRequest ("http://veiset.org:8183/" ++ show i)
+           request $ getRequest (domain ++ show t)
   pipe <- runIOE $ connect (M.host "127.0.0.1")
-  print i
   putStrLn "Parsing Measures"
   let mms = parseMeasures . B.pack . map c2w . rspBody $ rsp
   if (isNothing mms)
@@ -35,5 +54,7 @@ mainLoop i = do
         (Right _) -> do
            putStrLn "Done Putting to DB"
            M.close pipe
-           P.runCommand "sleep 10" >>= waitForProcess -- Workaround for veiset (noob)
-           mainLoop j
+           P.runCommand ("sleep " ++ show intrv) >>= waitForProcess -- Workaround for veiset (noob)
+           callback j
+
+mainLoop domain intrv t = mainBody t domain intrv (mainLoop domain intrv)
